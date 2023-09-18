97d7b37a2b71a9086dcec06a5418b3217492a483
Jaroslaw Weber <jaroslaw.weber@gmail.com>
2023-09-18 22:48:37 +0900

    change metadata to new title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'butter keys',
+  title: 'keyfab',
   description: 'keyboard layout design made easy',
 }
 
