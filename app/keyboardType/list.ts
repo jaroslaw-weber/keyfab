6d48d6858076a1eee1ca74429ff4ef56ebd48e29
Jaroslaw Weber <jaroslaw.weber@gmail.com>
2023-12-05 23:39:41 +0900

    add more keyboard types (from kle)

--- app/keyboardType/list.ts
+++ app/keyboardType/list.ts
@@ -3,16 +3,29 @@ import { moonlander } from "./moonlander";
 import { piantor } from "./piantor";
 
 import planc from "../physical/planc.json";
+import ansi104 from "../physical/ansi104.json";
+import iso105 from "../physical/iso105.json";
+import sixtyPercent from "../physical/sixtyPercent.json";
+import jd40 from "../physical/jd40.json";
+import keycool84 from "../physical/keycool84.json";
 
-const jsonLayouts = [planc]
+const jsonLayouts = [
+  //layouts from json
+  planc,
+  ansi104,
+  iso105,
+  sixtyPercent,
+  jd40,
+  keycool84
+];
 
-for(const l of jsonLayouts){
-validatePhysicalLayout(planc);
+for (const l of jsonLayouts) {
+  validatePhysicalLayout(planc);
 }
 
 export const keyboardTypes: KeyboardType[] = [
   piantor,
   moonlander,
   //layouts from json
-  ...jsonLayouts
+  ...jsonLayouts,
 ];
