9e257d5a86f64b19e6e6ff4048530eb2d1ccaff0
Jaroslaw Weber <jaroslawweber@gmail.com>
2024-07-07 23:32:12 +0900

    fix backend

--- app/db.ts
+++ app/db.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 
 const { POCKETBASE_URL } = process.env;
 console.log(POCKETBASE_URL)
-const database = new PocketBase("http://keyfab.jarek-backend.top:8090/");
+const database = new PocketBase("https://keyfab.jarek-backend.top/");
 database.authStore.loadFromCookie(document?.cookie ?? "");
 
 export const db = database
